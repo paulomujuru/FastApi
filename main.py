1106a0ed112df7555670e2fe25b0863d452f0d6a
Paulo Junior <79360295+paulomujuru@users.noreply.github.com>
2021-06-10 13:48:25 +0200

    Update to features to come...

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+'''
+This is project will hava a complete fastapi that will have all
+the features including:
+1. GET
+2. POST
+3. PATCH
+4. PUT
+5. DELETE
+And the authentication JWT and session id
+'''
+
 from typing import Optional
+
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -25,3 +37,6 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+
